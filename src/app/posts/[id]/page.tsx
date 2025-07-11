a03d0240e97a2a293cb7e83b9e390e3877a85573
haemin4738 <leeheamin12@gmail.com>
2025-07-11 16:02:30 +0900

    supabase 게시글 상세, 댓글

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,24 +1,44 @@
 "use client";
 
+import { supabase } from "@/app/lib/supabase";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 function PostDetail() {
+  const [post, setPost] = useState({});
+  const [comment, setComment] = useState({});
   const params = useParams();
   const { id } = params;
 
-  const [post, setPost] = useState({});
+  const fetchPost = async () => {
+    const { data, error } = await supabase
+      .from("posts")
+      .select("*")
+      .eq("id", id)
+      .single();
+    setPost(data);
+  };
+  const fetchComment = async () => {
+    const { data, error } = await supabase
+      .from("comments")
+      .select("*")
+      .eq("id", id)
+      .single();
+    setComment(data);
+  };
+
   useEffect(() => {
-    fetch(`https://dummyjson.com/posts/${id}`)
-      .then((res) => res.json())
-      .then((res) => setPost(res));
+    fetchPost();
+    fetchComment();
   }, []);
 
   return (
     <>
       <h1>{id}번</h1>
       <div className="text-2xl">{post.title}</div>
-      <p>{post.body}</p>
+      <p>{post.contents}</p>
+      <div className="text-2xl">{comment.id}</div>
+      <p className="text-xs">{comment.contents}</p>
     </>
   );
 }
