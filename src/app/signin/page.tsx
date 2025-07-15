96cf0034dd267331a6fbeb17d9b9956a8879689c
haemin4738 <leeheamin12@gmail.com>
2025-07-15 10:48:33 +0900

    로그인

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,46 +1,53 @@
 "use client";
 
 import { supabase } from "@/app/lib/supabase";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-function Signup() {
+function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const router = useRouter();
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const { error } = await supabase.auth.signUp({ email, password });
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
 
     if (error) {
       alert(error.message);
       console.log(error);
     } else {
-      alert("회원가입 성공");
+      alert("로그인 성공");
+      router.push("/");
     }
   };
 
   return (
     <>
-      <h1>회원가입</h1>
+      <h1>로그인</h1>
       <form onSubmit={handleSubmit}>
         <input
           type="email"
           name="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          placeholder="EMAIL"
+          placeholder="Email"
         />
         <input
           type="password"
           name="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          placeholder="PASSWORD"
+          placeholder="Password"
         />
-        <button type="submit">회원가입</button>
+        <button type="submit">로그인</button>
       </form>
     </>
   );
 }
 
-export default Signup;
+export default SignIn;
